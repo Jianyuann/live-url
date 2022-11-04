7c6cbc1339e93b6477071f870efdbb8b7be45225
jianyuan <jianyuann@gmail.com>
2022-11-04 09:57:27 +0800

    Create douyin.php

---
+++ douyin.php
@@ -0,0 +1,29 @@
+<?php
+
+$id = $_GET['id'];
+$rawUrl = 'https://live.douyin.com/webcast/web/enter/?aid=6383&web_rid='.$id;
+$headers = array(
+    'Cookie:ttwid=1%7CSgKP1ROhC9lf8AU5VR_bGvYOFXI5tlLJet4y-z2wb3s%7C1658845728%7C9ad3516d2604d385964001c61ff48275b9dacf7b7789bb1f78ae9dedf354206e',
+	'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0'
+	);
+	
+	$data = curlClient($rawUrl, $headers);
+	$json = json_decode($data, true);
+	$hls_url = $json['data']['data'][0]['stream_url']['hls_pull_url_map']['FULL_HD1'];
+	//$flv_url = $json['data']['data'][0]['stream_url']['flv_pull_url']['FULL_HD1'];
+	header('location:'.$hls_url);
+	
+function curlClient($url, $headers)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    if (!empty($headers)) {
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    }
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
