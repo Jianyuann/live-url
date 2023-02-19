a0ada7492225818cce7fb42f23d69ed237a17bc4
jianyuan <jianyuann@gmail.com>
2023-02-19 12:30:26 +0800

    Update douyin.php

--- douyin.php
+++ douyin.php
@@ -1,7 +1,7 @@
 <?php
 
 $id = $_GET['id'];
-$rawUrl = 'https://live.douyin.com/webcast/web/enter/?aid=6383&web_rid='.$id;
+$rawUrl = 'https://live.douyin.com/webcast/room/web/enter/?aid=6383&device_platform=web&web_rid='.$id;
 $headers = array(
     'Cookie:ttwid=1%7CSgKP1ROhC9lf8AU5VR_bGvYOFXI5tlLJet4y-z2wb3s%7C1658845728%7C9ad3516d2604d385964001c61ff48275b9dacf7b7789bb1f78ae9dedf354206e',
 	'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0'
