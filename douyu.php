99d23477237e63a36c0e91ce2a3cfcb8604bbb44
jianyuan <jianyuann@gmail.com>
2023-07-10 00:44:37 +0800

    Update douyu.php

--- douyu.php
+++ douyu.php
@@ -1,21 +1,19 @@
 <?php
-$id = $_GET[ 'id' ];
+    $id = $_GET['id'];
+    $ch = curl_init();
+    curl_setopt_array($ch,[
+  CURLOPT_URL => 'https://wxapp.douyucdn.cn/api/nc/stream/roomPlayer',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS =>"room_id=$id&big_ct=cpn-androidmpro&did=10000000000000000000000000001501&mt=2",
+]);
 
-$bstrURL = 'https://wxapp.douyucdn.cn/Livenc/Getplayer/newRoomPlayer';
-$postData = 'room_id=' . $id . '&token=wxapp&rate=1&did=10000000000000000000000000001501&big_ct=cpn-androidinpro&is_Mix=false';
-$ch = curl_init();
-curl_setopt( $ch, CURLOPT_URL, $bstrURL );
-curl_setopt( $ch, CURLOPT_RETURNTRANSFER, TRUE );
-curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, FALSE );
-curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, FALSE );
-curl_setopt( $ch, CURLOPT_POST, TRUE );
-curl_setopt( $ch, CURLOPT_USERAGENT, "Mozilla/5.0" );
-curl_setopt( $ch, CURLOPT_POSTFIELDS, $postData );
-$data = curl_exec( $ch );
-curl_close( $ch );
+    $playUrl = json_decode(curl_exec($ch))->data->live_url;
+    curl_close($ch);
+    header('location:'.$playUrl);
 
-$url = json_decode( $data )->data->hls_url;
-$hlsName = explode( '_', basename( parse_url( $url, PHP_URL_PATH ) ) );
-$playURL = 'https://openhls-tct.douyucdn2.cn/live/' . $hlsName[ 0 ] . '.m3u8';
-header( 'location:' . $playURL );
 ?>
