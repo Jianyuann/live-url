48c28164dd06971e9fed13301b577f588422a06a
jianyuan <jianyuann@gmail.com>
2023-09-07 22:07:13 +0800

    Update bili.php

--- bili.php
+++ bili.php
@@ -1,36 +1,24 @@
 <?php
-$id = $_GET[ 'id' ];
+$id = $_GET['id'] ?? '23';
 
-$apiUrl='https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo?protocol=1&format=1&codec=0&qn=30000&platform=web&room_id='. $id;
+$apiUrl = 'https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo?protocol=1&format=1&codec=0&qn=30000&platform=web&room_id=' . $id;
 
-$rawUrl=json_decode(get_data($apiUrl,null,null),true);
+$json = json_decode(get_data($apiUrl), true);
+$codec = $json['data']['playurl_info']['playurl']['stream'][0]['format'][0]['codec'][0];
+$playUrl = $codec['url_info'][0]['host'] . $codec['base_url'] . $codec['url_info'][0]['extra'];
 
-$host=$rawUrl['data']['playurl_info']['playurl']['stream'][0]['format'][0]['codec'][0]['url_info'][0]['host'];
-$baseUrl=$rawUrl['data']['playurl_info']['playurl']['stream'][0]['format'][0]['codec'][0]['base_url'];
-$extra=$rawUrl['data']['playurl_info']['playurl']['stream'][0]['format'][0]['codec'][0]['url_info'][0]['extra'];
-
-$playUrl=$host.$baseUrl.$extra;
-header('location:'.$playUrl);
-
-function get_data($url,$headers, $payload): bool|string
+header('location:' . $playUrl);
+function get_data($url): bool|string
 {
     $curl = curl_init();
-    if(str_starts_with($url, 'https')){
-        curl_setopt( $curl, CURLOPT_SSL_VERIFYPEER, FALSE );
-        curl_setopt( $curl, CURLOPT_SSL_VERIFYHOST, FALSE );
+    if (str_starts_with($url, 'https')) {
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
     }
-    curl_setopt( $curl, CURLOPT_RETURNTRANSFER, true );
-    curl_setopt( $curl, CURLOPT_URL, $url );
-    if ( !empty( $payload ) ) {
-        curl_setopt( $curl, CURLOPT_POST, 1 );
-        curl_setopt( $curl, CURLOPT_CUSTOMREQUEST, "POST");
-        curl_setopt( $curl, CURLOPT_POSTFIELDS, $payload );
-    }
-    if ( !empty( $headers ) ) {
-        curl_setopt( $curl, CURLOPT_HTTPHEADER, $headers );
-    }
-    curl_setopt ($curl, CURLOPT_CONNECTTIMEOUT, 5);
-    $data = curl_exec( $curl );
-    curl_close( $curl );
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 5);
+    $data = curl_exec($curl);
+    curl_close($curl);
     return $data;
 }
