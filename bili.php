5c7ee92a6d313312a962ddb8e8167926f92c627b
jianyuan <jianyuann@gmail.com>
2024-03-05 09:41:23 +0800

    Update bili.php

--- bili.php
+++ bili.php
@@ -1,7 +1,6 @@
 <?php
 $id = $_GET['id'] ?? '23';
-$apiUrl = 
-'https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo?protocol=1&format=1&codec=0&room_id='.$id;
+$apiUrl = 'https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo?protocol=1&format=1&codec=0&room_id='.$id;
 $codec = json_decode(file_get_contents($apiUrl), true)['data']['playurl_info']['playurl']['stream'][0]['format'][0]['codec'][0];
-$playUrl = $codec['url_info'][0]['host'] . preg_replace('/_([a-zA-Z0-9]+)\.m3u8/', '.m3u8', $codec['base_url']). $codec['url_info'][0]['extra'];
+$playUrl = $codec['url_info'][0]['host']. preg_replace('/(live_\d+_\d+)(_\w+)?(\.m3u8\?)$/ ','$1$3', $codec['base_url']). $codec['url_info'][0]['extra'];
 header('location:' . $playUrl);
