5356b63bc8ebfa41d9a480dd19f7479b86f22d35
jianyuan <jianyuann@gmail.com>
2024-08-02 16:10:13 +0800

    Update douyu.php

--- douyu.php
+++ douyu.php
@@ -1,9 +1,8 @@
 <?php
+date_default_timezone_set("Asia/Shanghai");
 $id = $_GET['id'];
 $apiUrl = 'https://wxapp.douyucdn.cn/Livenc/Getplayer/newRoomPlayer';
-
 $data = file_get_contents($apiUrl, false, stream_context_create(array('http'=>array('method'=>'POST', 'content'=>http_build_query(array('room_id'=>$id, 'token'=>1))))));
-
 $rawUrl = json_decode($data)->data->hls_url;
-$playUrl = 'https://tc-tc2-interact.douyucdn2.cn/live/' . explode('_', basename($rawUrl))[0] . '.m3u8';
+$playUrl = 'https://openhls-tct.douyucdn2.cn/live/' . explode('_', basename($rawUrl))[0] . '.m3u8';
 header('location:' . $playUrl);
