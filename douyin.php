b5247b69b6f8eb943c2da12a7764762d040a79cb
jianyuan <jianyuann@gmail.com>
2024-08-29 16:51:20 +0800

    更新抖音，默认最高画质

--- douyin.php
+++ douyin.php
@@ -1,30 +1,10 @@
 <?php
-
 $id = $_GET['id'];
-$rawUrl ='https://live.douyin.com/webcast/room/web/enter/?aid=6383&app_name=douyin_web&live_id=1&device_platform=web&language=zh-CN&cookie_enabled=true&screen_width=1280&screen_height=720&browser_language=zh-CN&browser_platform=Win32&browser_name=Firefox&browser_version=113.0&web_rid='.$id;
-
-$headers = array(
-    'Cookie:ttwid=1%7CSgKP1ROhC9lf8AU5VR_bGvYOFXI5tlLJet4y-z2wb3s%7C1658845728%7C9ad3516d2604d385964001c61ff48275b9dacf7b7789bb1f78ae9dedf354206e',
-	'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0'
-	);
-	
-	$data = curlClient($rawUrl, $headers);
-	$json = json_decode($data, true);
-	$hls_url = $json['data']['data'][0]['stream_url']['hls_pull_url_map']['FULL_HD1'];
-	//$flv_url = $json['data']['data'][0]['stream_url']['flv_pull_url']['FULL_HD1'];
-	header('location:'.$hls_url);
-	
-function curlClient($url, $headers)
-{
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-    if (!empty($headers)) {
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    }
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-    $data = curl_exec($ch);
-    curl_close($ch);
-    return $data;
-}
+$apiUrl = 'https://live.douyin.com/webcast/room/web/enter/?aid=6383&device_platform=web&browser_language=zh-CN&browser_platform=iPad&browser_name=Safari&browser_version=17.6&web_rid=' . $id;
+$options = ['http' => ['method' => 'GET', 'header' => ['Cookie: ttwid=1%7CYqdH7G5MetcLz-SYn5w09SR81BSavm6lW81orliZUbA%7C1719753328%7Ccb83acfa23ce88b920c26fef1746820e6a701528adfe7c48847cfb5925eb1a0f']]];
+$json = json_decode(file_get_contents($apiUrl, false, stream_context_create($options)), true);
+$stream_data_json = $json['data']['data'][0]['stream_url']['live_core_sdk_data']['pull_data']['stream_data'];
+$stream_data = json_decode($stream_data_json, true);
+$hls_url = $stream_data['data']['origin']['main']['hls'];
+//$flv_url = $stream_data['data']['origin']['main']['flv'];
+header('location:' . $hls_url);
