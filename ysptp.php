77812a5e05eb1499a0c7c6b0612ca9ee3c381818
jianyuan <jianyuann@gmail.com>
2024-09-07 15:32:20 +0800

    Update ysptp.php

--- ysptp.php
+++ ysptp.php
@@ -1,10 +1,12 @@
 <?php
-$id = $_GET['id'] ?? '8k120m';
+$id = $_GET['id'] ?? 'cctv1';
+
+$baseUrl = 'http://live-tpgq.cctv.cn/live/' . $id . '.m3u8';
+$path = 'http://livews-tpgq.cctv.cn/live/';
 
 if (str_contains($id, '8k')) {
-    $baseUrl = 'http://livews-tp4k.cctv.cn/live/' . ($id === '8k120m' ? '8K120M/playlist.m3u8' : '4K36M/playlist.m3u8');
-} else {
-    $baseUrl = 'http://livews-tpgq.cctv.cn/live/' . $id . '.m3u8';
+    $baseUrl = 'http://livews-tp4k.cctv.cn/live/' . ($id === 'cctv8k120m' ? '8K120M/playlist.m3u8' : ($id === 'cctv8k60m' ? '4K60M/playlist.m3u8' : '4K36M/playlist.m3u8'));
+    $path = 'http://livews-tp4k.cctv.cn/live/' . ($id === 'cctv8k120m' ? '8K120M/' : ($id === 'cctv8k60m' ? '4K60M/' : '4K36M/'));
 }
 
 $apiUrl = "https://ytpvdn.cctv.cn/cctvmobileinf/rest/cctv/videoliveUrl/getstream";
@@ -12,11 +14,7 @@ $headers = ["User-Agent: cctv_app_tv", "Referer: api.cctv.cn", "UID: 1234123122"
 $payload = http_build_query(['appcommon' => json_encode(['ap' => 'cctv_app_tv', 'an' => '央视投屏助手', 'adid' => '1234123122', 'av' => '1.1.7']), 'url' => $baseUrl]);
 
 $playUrl = json_decode(file_get_contents($apiUrl, false, stream_context_create(['http' => ['header' => implode("\r\n", $headers), 'method' => 'POST', 'content' => $payload,]])))->url;
-$liveUrl = preg_replace('/(.*?.ts)/i', str_replace('playlist.m3u8', '', $baseUrl) . '$1', file_get_contents($playUrl, false, stream_context_create(['http' => ['header' => implode("\r\n", $headers)]])));
-
-if (!str_contains($id, '8k')) {
-    $liveUrl = str_replace($id . '.m3u8', '', $liveUrl);
-}
+$liveUrl = preg_replace('/(.*?.ts)/i', $path . '$1', file_get_contents($playUrl, false, stream_context_create(['http' => ['header' => implode("\r\n", $headers)]])));
 
 header("Content-Type: application/vnd.apple.mpegurl");
 header("Content-Disposition: attachment; filename=" . $id . ".m3u8");
