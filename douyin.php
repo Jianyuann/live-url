404f727152beebb31d0cfebf70ee44674bd3bfe5
jianyuan <jianyuann@gmail.com>
2025-09-03 17:10:39 +0800

    Update douyin.php

--- douyin.php
+++ douyin.php
@@ -1,6 +1,6 @@
 <?php
 $id = $_GET['id'];
-$apiUrl = 'https://live.douyin.com/webcast/room/web/enter/?aid=6383&device_platform=web&browser_language=zh-CN&browser_platform=iPad&browser_name=Safari&browser_version=17.6&web_rid=' . $id;
+$apiUrl = 'https://live.douyin.com/webcast/room/web/enter/?aid=6383&a_bogus=1&device_platform=web&browser_language=zh-CN&browser_platform=iPad&browser_name=Safari&browser_version=17.6&web_rid=' . $id;
 $options = ['http' => ['method' => 'GET', 'header' => ['Cookie: ttwid=1%7CYqdH7G5MetcLz-SYn5w09SR81BSavm6lW81orliZUbA%7C1719753328%7Ccb83acfa23ce88b920c26fef1746820e6a701528adfe7c48847cfb5925eb1a0f']]];
 $json = json_decode(file_get_contents($apiUrl, false, stream_context_create($options)), true);
 $stream_data_json = $json['data']['data'][0]['stream_url']['live_core_sdk_data']['pull_data']['stream_data'];
