e3ea8c03ebeed9610e4f55eadfe3024e55056bab
jianyuan <jianyuann@gmail.com>
2025-10-17 17:57:03 +0800

    Update huya.php

--- huya.php
+++ huya.php
@@ -1,22 +1,40 @@
 <?php
-date_default_timezone_set("Asia/Shanghai");
-$rid = $_GET['id'] ?? '';
-$cdn = $_GET['cdn'] ?? 'tx'; // 默认 tx，可选 al/hs/hw
-$uid = '1560173900';
 
-$apiUrl = "https://mp.huya.com/cache.php?m=Live&do=profileRoom&roomid=$rid";
-$info = json_decode(file_get_contents($apiUrl), true)["data"]["stream"]["baseSteamInfoList"][0];
-$streamName = $info["sStreamName"];
-parse_str($info["sFlvAntiCode"], $p);
+declare(strict_types=1);
 
-$ctype = 'tars_wap'; $fs = $p['fs']; $ver = 1; $t = 102;
+$roomId = $_GET['id'] ?? '';
+$cdn = $_GET['cdn'] ?? 'tx'; // tx, al, hs, hw
+
+$apiUrl = "https://mp.huya.com/cache.php?m=Live&do=profileRoom&roomid=$roomId";
+$infoData = json_decode(file_get_contents($apiUrl), true);
+$streamInfo = $infoData['data']['stream']['baseSteamInfoList'][0];
+$streamName = $streamInfo['sStreamName'];
+
+parse_str($streamInfo['sFlvAntiCode'] ?? '', $antiCodeParams);
+
+$userId = '1560173900';
+$contentType = 'tars_wap';
+$type = 102;
 $wsTime = dechex(time() + 3600);
-$seqid = (int)(microtime(true) * 1000);
-$hash = md5("$seqid|$ctype|$t");
-$wsSecret = md5("{$uid}_{$streamName}_{$hash}_$wsTime");
+$seqId = (string) (int) (microtime(true) * 1000);
+$hash = md5("$seqId|$contentType|$type");
+$fmEncoded = $antiCodeParams['fm'] ?? '';
+$fmDecoded = base64_decode(urldecode($fmEncoded));
+$fmReplaced = str_replace('$0', $userId, $fmDecoded);
+$fmReplaced = str_replace('$1', $streamName, $fmReplaced);
+$fmReplaced = str_replace('$2', $hash, $fmReplaced);
+$fmReplaced = str_replace('$3', $wsTime, $fmReplaced);
+$wsSecret = md5($fmReplaced);
+$queryParams = [
+    'wsSecret' => $wsSecret,
+    'wsTime' => $wsTime,
+    'ctype' => $contentType,
+    'seqid' => $seqId,
+    'uid' => $userId,
+    'fs' => $antiCodeParams['fs'] ?? '',
+    'ver' => 1,
+    't' => $type,
+];
 
-$playUrl = "http://$cdn.flv.huya.com/src/$streamName.flv?" . http_build_query([
-        'wsSecret'=>$wsSecret,'wsTime'=>$wsTime,'ctype'=>$ctype,'seqid'=>$seqid,
-        'uid'=>$uid,'fs'=>$fs,'ver'=>$ver,'t'=>$t
-    ]);
+$playUrl = "https://$cdn.flv.huya.com/src/$streamName.flv?" . http_build_query($queryParams);
 header("Location: $playUrl");
