e5afb2f2de8f724992a576abc15fb6cd5c17bfad
jianyuan <jianyuann@gmail.com>
2025-10-17 21:49:08 +0800

    Update douyin.php

--- douyin.php
+++ douyin.php
@@ -1,17 +1,13 @@
 <?php
 
-declare(strict_types=1);
-
 $webcastId = $_GET['id'] ?? '339638082961';
-
-$userAgent = 'Mozilla/5.0 (iPhone; CPU iPhone OS 18_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.7 Mobile/15E148 Safari/604.1';
 
 $roomUrl = "https://live.douyin.com/$webcastId";
 
 $context = stream_context_create([
     'http' => [
         'method' => 'GET',
-        'header' => "User-Agent: $userAgent\r\n",
+        'header' => "User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 18_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.7 Mobile/15E148 Safari/604.1\r\n",
         'follow_location' => 0,
     ],
 ]);
@@ -20,8 +16,15 @@ file_get_contents($roomUrl, false, $context);
 
 $headers = $http_response_header ?? [];
 
-$redirectUrl = trim(substr($header, strlen('Location:')));
+$redirectUrl = '';
+foreach ($headers as $header) {
+    if (stripos($header, 'Location:') === 0) {
+        $redirectUrl = trim(substr($header, strlen('Location:')));
+        break;
+    }
+}
 
+$redirectUrl !== '' or exit;
 $urlParts = parse_url($redirectUrl);
 $pathParts = explode('/', trim($urlParts['path'], '/'));
 $roomId = end($pathParts);
@@ -30,30 +33,11 @@ $secUserId = $queryParams['sec_user_id'] ?? '';
 
 $apiUrl = "https://webcast.amemv.com/webcast/room/reflow/info/?type_id=0&live_id=1&room_id=$roomId&sec_user_id=$secUserId&app_id=1128";
 
-$context = stream_context_create([
-    'http' => [
-        'header' => "User-Agent: userAgent\r\n",
-    ],
-]);
-
-$responseBody = file_get_contents($apiUrl, false, $context);
-
-$jsonData = json_decode($responseBody, true);
-
-$roomData = $jsonData['data']['room'];
-
-$streamDataJson = $roomData['stream_url']['live_core_sdk_data']['pull_data']['stream_data'];
-
-$streamData = json_decode($streamDataJson, true);
-
-$mainStream = $streamData['data']['origin']['main'] ?? [];
-
-$flvUrlMap = $roomData['stream_url']['flv_pull_url_map'] ?? [];
-$firstKey = array_key_first($flvUrlMap) ?? null;
-
-$flvUrl = $mainStream['flv'] ?? ($flvUrlMap[$firstKey] ?? '');
-
-header('Location: ' . $flvUrl);
+$stream_data_json = json_decode(file_get_contents($apiUrl, false, $context), true)['data']['room']['stream_url']['live_core_sdk_data']['pull_data']['stream_data'];
+$stream_data = json_decode($stream_data_json, true);
+$hls_url = $stream_data['data']['origin']['main']['hls'];
+$flv_url = $stream_data['data']['origin']['main']['flv'];
+header('location:' . $flv_url);
 
 /*
 <?php
