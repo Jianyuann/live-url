0e2b7f54d81f3c3cf58217a75bde21aba45bc9fc
jianyuan <jianyuann@gmail.com>
2026-03-11 11:45:42 +0800

    Refactor Douyu API integration and remove unused code

--- douyu.php
+++ douyu.php
@@ -1,70 +1,51 @@
 <?php
-
 declare(strict_types=1);
 
-use Random\RandomException;
-
 $roomId = $_GET['id'] ?? '999';
-$timestamp = (string)time();
 $deviceId = '10000000000000000000000000001501';
-$rate = '0';
-$cdn = '';
+$timestamp = (string)time();
 
-$pageContent = file_get_contents('https://www.douyu.com/' . $roomId);
+$encryptionUrl = 'https://www.douyu.com/wgapi/livenc/liveweb/websec/getEncryption?did=' . $deviceId;
+$encryptionData = json_decode(file_get_contents($encryptionUrl), true);
 
-preg_match('/(vdwdae325w_64we[\s\S]*?function\s+ub98484234[\s\S]*?)function/', $pageContent, $matches) || exit;
-
-$patchedJs = preg_replace('/eval.*?;}/s', 'strc;}', $matches[1]);
-
-try {
-    $tempJsFile = sys_get_temp_dir() . DIRECTORY_SEPARATOR . ('dy_' . bin2hex(random_bytes(8)) . '.js');
-} catch (RandomException $e) {
-
+if (!$encryptionData || ($encryptionData['error'] ?? 1) !== 0) {
+    die('Failed to get encryption data');
 }
-$envRoomId = escapeshellarg($roomId);
-$envDeviceId = escapeshellarg($deviceId);
-$envTimestamp = escapeshellarg($timestamp);
-$envPatchedJs = base64_encode($patchedJs);
 
-$nodeScript = <<<NODE
-    const crypto = require('crypto');
-    const rid = process.env.RID || '';
-    const did = process.env.DID || '';
-    const tt  = process.env.TT  || '';
-    const ub9_b64 = process.env.UB9 || '';
-    var window={},document={},navigator={},console={log:function(){}};
-    
-    try{
-      const code = Buffer.from(ub9_b64,'base64').toString('utf8');
-      eval(code); // define ub98484234()
-      var res = ub98484234(); 
-      var m   = /v=(\\d+)/.exec(res);
-      var v   = m ? m[1] : '';
-      var rb  = crypto.createHash('md5').update(rid + did + tt + v).digest('hex');
-    
-      var func = res.replace(/return rt;\\}\\);?/, 'return rt;}');
-      func = func.replace('(function (','function sign(')
-                 .replace('CryptoJS.MD5(cb).toString()', "'" + rb + "'");
-      eval(func); 
-      var out = (typeof sign==='function') ? sign(rid,did,tt) : '';
-      process.stdout.write(out);
-    } catch(e){
-      process.stdout.write('');
-    }
-NODE;
+$encData = $encryptionData['data'];
+$key = $encData['key'];
+$randStr = $encData['rand_str'];
+$encTime = $encData['enc_time'];
+$isSpecial = $encData['is_special'] ?? 0;
+$encDataValue = $encData['enc_data'];
 
-file_put_contents($tempJsFile, $nodeScript);
-$command = "RID=$envRoomId DID=$envDeviceId TT=$envTimestamp UB9=" . escapeshellarg($envPatchedJs) . ' node ' . escapeshellarg($tempJsFile);
-$postParams = (string)shell_exec($command);
-@unlink($tempJsFile);
+$i = ($isSpecial === 1) ? '' : $roomId . $timestamp;
+$f = $randStr;
+for ($p = 0; $p < $encTime; $p++) {
+    $f = md5($f . $key);
+}
+$auth = md5($f . $key . $i);
 
-$postParams .= '&cdn=' . rawurlencode($cdn) . '&rate=' . rawurlencode($rate);
+$rate = '0';
+$postParams = http_build_query([
+    'enc_data' => $encDataValue,
+    'tt' => $timestamp,
+    'did' => $deviceId,
+    'auth' => $auth,
+    'cdn' => '',
+    'rate' => $rate,
+    'hevc' => '0',
+    'fa' => '0',
+    'ive' => '0'
+]);
 
-$apiUrl = 'https://www.douyu.com/lapi/live/getH5Play/' . $roomId;
+$apiUrl = 'https://www.douyu.com/lapi/live/getH5PlayV1/' . $roomId;
+
 $context = stream_context_create([
     'http' => [
         'method' => 'POST',
         'content' => $postParams,
+        'header' => "Content-Type: application/x-www-form-urlencoded\r\n"
     ],
 ]);
 $jsonData = json_decode(file_get_contents($apiUrl, false, $context), true);
@@ -75,14 +56,3 @@ $rtmpLive = (string)($jsonData['data']['rtmp_live'] ?? '');
 $streamUrl = rtrim($rtmpUrl, '/') . '/' . ltrim($rtmpLive, '/');
 
 header('Location: ' . $streamUrl);
-
-/*<?php
-date_default_timezone_set("Asia/Shanghai");
-$id = $_GET['id'];
-$apiUrl = 'https://wxapp.douyucdn.cn/Livenc/Getplayer/newRoomPlayer';
-$data = file_get_contents($apiUrl, false, stream_context_create(array('http'=>array('method'=>'POST', 'content'=>http_build_query(array('room_id'=>$id, 'token'=>1))))));
-$rawUrl = json_decode($data)->data->hls_url;
-//tc-tct、open-tct、alia
-$playUrl = 'https://alia.douyucdn2.cn/live/' . explode('_', basename($rawUrl))[0] . '.m3u8';
-header('location:' . $playUrl);
-*/
