30f021c73e13da2da4c4c09d98057175b1171783
jianyuan <jianyuann@gmail.com>
2026-03-11 15:10:03 +0800

    Refactor encryption data retrieval in douyu.php

--- douyu.php
+++ douyu.php
@@ -5,54 +5,47 @@ $roomId = $_GET['id'] ?? '999';
 $deviceId = '10000000000000000000000000001501';
 $timestamp = (string)time();
 
-$encryptionUrl = 'https://www.douyu.com/wgapi/livenc/liveweb/websec/getEncryption?did=' . $deviceId;
-$encryptionData = json_decode(file_get_contents($encryptionUrl), true);
+$encUrl = 'https://www.douyu.com/wgapi/livenc/liveweb/websec/getEncryption?did=' . $deviceId;
+$encData = json_decode(file_get_contents($encUrl), true)['data'] ?? [];
 
-if (!$encryptionData || ($encryptionData['error'] ?? 1) !== 0) {
-    die('Failed to get encryption data');
-}
+extract($encData); 
 
-$encData = $encryptionData['data'];
-$key = $encData['key'];
-$randStr = $encData['rand_str'];
-$encTime = $encData['enc_time'];
-$isSpecial = $encData['is_special'] ?? 0;
-$encDataValue = $encData['enc_data'];
+$isSpecial = (int)($is_special ?? 0);
+$encTime   = (int)($enc_time ?? 0);
 
 $i = ($isSpecial === 1) ? '' : $roomId . $timestamp;
-$f = $randStr;
+$f = $rand_str;
 for ($p = 0; $p < $encTime; $p++) {
     $f = md5($f . $key);
 }
 $auth = md5($f . $key . $i);
 
-$rate = '0';
 $postParams = http_build_query([
-    'enc_data' => $encDataValue,
-    'tt' => $timestamp,
-    'did' => $deviceId,
-    'auth' => $auth,
-    'cdn' => '',
-    'rate' => $rate,
-    'hevc' => '0',
-    'fa' => '0',
-    'ive' => '0'
+    'enc_data' => $enc_data,
+    'tt'       => $timestamp,
+    'did'      => $deviceId,
+    'auth'     => $auth,
+    'cdn'      => '',
+    'rate'     => '0',
+    'hevc'     => '0',
+    'fa'       => '0',
+    'ive'      => '0'
 ]);
 
 $apiUrl = 'https://www.douyu.com/lapi/live/getH5PlayV1/' . $roomId;
-
 $context = stream_context_create([
     'http' => [
-        'method' => 'POST',
+        'method'  => 'POST',
         'content' => $postParams,
-        'header' => "Content-Type: application/x-www-form-urlencoded\r\n"
-    ],
+        'header'  => "Content-Type: application/x-www-form-urlencoded\r\n"
+    ]
 ]);
-$jsonData = json_decode(file_get_contents($apiUrl, false, $context), true);
 
-$rtmpUrl = (string)($jsonData['data']['rtmp_url'] ?? '');
-$rtmpLive = (string)($jsonData['data']['rtmp_live'] ?? '');
+$json = json_decode(file_get_contents($apiUrl, false, $context), true);
+$data = $json['data'] ?? [];
 
+$rtmpUrl = (string)($data['rtmp_url'] ?? '');
+$rtmpLive = (string)($data['rtmp_live'] ?? '');
 $streamUrl = rtrim($rtmpUrl, '/') . '/' . ltrim($rtmpLive, '/');
 
 header('Location: ' . $streamUrl);
